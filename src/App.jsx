0c4914c2a34508527a98c8bbda23aed1a40a10aa
jacquelin <jacquelinjantman@gmail.com>
2026-06-12 20:53:37 -0300

    size gif

--- src/App.jsx
+++ src/App.jsx
@@ -151,7 +151,7 @@ const removeTask = (id) => {
          
         </div>
       </div>
-       <img src={mascot} alt="mascota" className="mascota" style={{width: "400px", height: "400px", objectFit: "contain", position: "absolute", bottom: "10rem", right: "8rem"}}/>
+       <img src={mascot} alt="mascota" className="mascota" style={{width: "150px", height: "150px", objectFit: "contain", position: "absolute", bottom: "10rem", right: "8rem"}}/>
     </div>
   </div>
 
